e81661e6cbfa2735b15a2e1c772b012f9e2870bb
mfred <6153204+mfred488@users.noreply.github.com>
2020-01-10 07:23:12 +0900

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 enum Object {
-    FERRIS
+    FERRIS,
 }
 
-fn get_printable_character (object: &Object) -> String {
+fn get_printable_character(object: &Object) -> String {
     match object {
         Object::FERRIS => return String::from("🦀"),
     };
@@ -25,20 +25,17 @@ impl Level {
         Level {
             width,
             height,
-            objects_with_locations: Vec::new()
+            objects_with_locations: Vec::new(),
         }
     }
 
     fn add_object(&mut self, x: usize, y: usize, object: Object) {
-        self.objects_with_locations.push(ObjectWithLocation {
-            x,
-            y,
-            object,
-        })
+        self.objects_with_locations
+            .push(ObjectWithLocation { x, y, object })
     }
 
     fn print(&self) {
-        let mut characters : Vec<Vec<String>> = Vec::with_capacity(self.width);
+        let mut characters: Vec<Vec<String>> = Vec::with_capacity(self.width);
         for _ in 0..self.width {
             let mut line = Vec::with_capacity(self.height);
             for _ in 0..self.height {
@@ -48,7 +45,8 @@ impl Level {
         }
 
         for object_with_location in &self.objects_with_locations {
-            characters[object_with_location.x][object_with_location.y] = get_printable_character(&object_with_location.object);
+            characters[object_with_location.x][object_with_location.y] =
+                get_printable_character(&object_with_location.object);
         }
 
         for y in 0..self.height {
@@ -63,9 +61,8 @@ impl Level {
 
 fn main() {
     let mut level = Level::new(10, 10);
-    level.add_object(2,3, Object::FERRIS);
-    level.add_object(6,6, Object::FERRIS);
+    level.add_object(2, 3, Object::FERRIS);
+    level.add_object(6, 6, Object::FERRIS);
     //println!("{}", get_printable_character(&Object::FERRIS));
     level.print();
 }
-
