c803cb9384170695bbfa0913c71451e9394150d6
mfred <6153204+mfred488@users.noreply.github.com>
2020-01-12 00:36:53 +0900

    implement is stop

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use termion::raw::IntoRawMode;
 enum Object {
     FERRIS,
     ROCKET,
+    FLAG,
+    WALL,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
@@ -23,12 +25,15 @@ enum Word {
 enum Noun {
     FERRIS,
     ROCKET,
+    FLAG,
+    WALL,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 enum Adjective {
     YOU,
     WIN,
+    STOP,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
@@ -48,16 +53,22 @@ fn get_printable_character(element: Option<&Element>) -> String {
     match element {
         Some(Element::Object(Object::FERRIS)) => return String::from("🦀"),
         Some(Element::Object(Object::ROCKET)) => return String::from("🚀"),
+        Some(Element::Object(Object::FLAG)) => return String::from("🚩"),
+        Some(Element::Object(Object::WALL)) => return String::from("🧱"),
         Some(Element::Word(Word::Noun(Noun::FERRIS))) => return String::from("Fe"),
         Some(Element::Word(Word::Noun(Noun::ROCKET))) => return String::from("Ro"),
+        Some(Element::Word(Word::Noun(Noun::FLAG))) => return String::from("Fl"),
+        Some(Element::Word(Word::Noun(Noun::WALL))) => return String::from("Wa"),
         Some(Element::Word(Word::IS)) => return String::from("=="),
         Some(Element::Word(Word::Adjective(Adjective::YOU))) => return String::from("U "),
         Some(Element::Word(Word::Adjective(Adjective::WIN))) => return String::from("Wi"),
+        Some(Element::Word(Word::Adjective(Adjective::STOP))) => return String::from("St"),
         None => return String::from(".."),
         // _ => return String::from("?"),
     };
 }
 
+#[derive(Debug)]
 struct ElementWithLocation {
     x: usize,
     y: usize,
@@ -75,6 +86,8 @@ fn get_noun(object: &Object) -> Noun {
     match object {
         Object::FERRIS => Noun::FERRIS,
         Object::ROCKET => Noun::ROCKET,
+        Object::FLAG => Noun::FLAG,
+        Object::WALL => Noun::WALL,
     }
 }
 
@@ -120,23 +133,12 @@ impl Level {
             let mut new_x = element_with_location.x;
             let mut new_y = element_with_location.y;
             if self.is_adjective(&element_with_location.element, Adjective::YOU) {
-                match direction {
-                    Direction::UP => new_y = if new_y == 0 { 0 } else { new_y - 1 },
-                    Direction::DOWN => {
-                        new_y = if new_y == self.height - 1 {
-                            self.height - 1
-                        } else {
-                            new_y + 1
-                        }
+                match self.can_move(&element_with_location, &direction) {
+                    Some((local_new_x, local_new_y)) => {
+                        new_x = local_new_x;
+                        new_y = local_new_y;
                     }
-                    Direction::RIGHT => {
-                        new_x = if new_x == self.width - 1 {
-                            self.width - 1
-                        } else {
-                            new_x + 1
-                        }
-                    }
-                    Direction::LEFT => new_x = if new_x == 0 { 0 } else { new_x - 1 },
+                    None => {}
                 }
             }
 
@@ -155,6 +157,55 @@ impl Level {
         }
 
         self.is_win()
+    }
+
+    fn can_move(
+        &self,
+        element_with_location: &ElementWithLocation,
+        direction: &Direction,
+    ) -> Option<(usize, usize)> {
+        let mut new_x = element_with_location.x;
+        let mut new_y = element_with_location.y;
+        // Objects can't go off limits
+        match direction {
+            Direction::UP => {
+                if new_y > 0 {
+                    new_y = new_y - 1
+                } else {
+                    return None;
+                }
+            }
+            Direction::DOWN => {
+                if new_y < self.height - 1 {
+                    new_y = new_y + 1
+                } else {
+                    return None;
+                }
+            }
+            Direction::LEFT => {
+                if new_x > 0 {
+                    new_x = new_x - 1
+                } else {
+                    return None;
+                }
+            }
+            Direction::RIGHT => {
+                if new_x < self.width - 1 {
+                    new_x = new_x + 1
+                } else {
+                    return None;
+                }
+            }
+        }
+
+        // Can't pass stop objects
+        for element_in_next_location in &self.grid[self.get_grid_index(new_x, new_y)] {
+            if self.is_adjective(&element_in_next_location, Adjective::STOP) {
+                return None;
+            }
+        }
+
+        Some((new_x, new_y))
     }
 
     fn is_adjective(&self, element: &Element, adjective: Adjective) -> bool {
@@ -176,6 +227,10 @@ impl Level {
                                             if noun == &get_noun(&object)
                                                 && &adjective == local_adjective
                                             {
+                                                eprintln!(
+                                                    "{:?} is {:?} (vertical rule at {},{})",
+                                                    object, adjective, x, y
+                                                );
                                                 return true;
                                             } else {
                                                 continue;
@@ -241,7 +296,7 @@ impl Level {
     }
 
     fn print(&self, stdout: &mut termion::raw::RawTerminal<std::io::Stdout>) {
-        write!(stdout, "{}", termion::clear::All).unwrap();
+        // write!(stdout, "{}", termion::clear::All).unwrap();
         for y in 0..self.height {
             let mut line = String::with_capacity(self.width);
             for x in 0..self.width {
@@ -256,6 +311,12 @@ impl Level {
             )
             .unwrap();
         }
+        write!(
+            stdout,
+            "{}",
+            termion::cursor::Goto(1, (1 + self.height).try_into().unwrap())
+        )
+        .unwrap();
         stdout.flush().unwrap();
     }
 }
@@ -263,6 +324,8 @@ impl Level {
 fn main() {
     let mut level = Level::new(12, 12);
     level.add_object(2, 3, Element::Object(Object::FERRIS));
+    level.add_object(1, 9, Element::Object(Object::FLAG));
+    level.add_object(0, 0, Element::Object(Object::WALL));
     level.add_object(6, 6, Element::Object(Object::FERRIS));
     level.add_object(0, 5, Element::Object(Object::ROCKET));
     level.add_object(7, 9, Element::Word(Word::Noun(Noun::FERRIS)));
@@ -271,6 +334,9 @@ fn main() {
     level.add_object(7, 10, Element::Word(Word::Noun(Noun::ROCKET)));
     level.add_object(8, 10, Element::Word(Word::IS));
     level.add_object(9, 10, Element::Word(Word::Adjective(Adjective::WIN)));
+    level.add_object(7, 11, Element::Word(Word::Noun(Noun::WALL)));
+    level.add_object(8, 11, Element::Word(Word::IS));
+    level.add_object(9, 11, Element::Word(Word::Adjective(Adjective::STOP)));
 
     let stdin = stdin();
     let mut stdout = stdout().into_raw_mode().unwrap();
